37735905687952882e4a0a459617bf528251806b
le gabonais <inf3494@hs-worms.de>
2021-05-11 22:46:42 +0200

    new model "Question"

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Frage(models.Model): 
+    id = models.IntegerField(primary_key=True)
+    themaId = models.IntegerField()
+    userId = models.IntegerField()
+
