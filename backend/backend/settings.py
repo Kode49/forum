5c2c70bcdc413bfe20a67a787fd1d55877a22046
Marc Pekoua <marc.pekoua@futurenext.de>
2021-05-13 13:44:51 +0200

    #1: set postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'forum_code49',
+        'USER': 'code49',
+        'PASSWORD': 'Kode!sfun49',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
