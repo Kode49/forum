007f26ab93f822e528bdbe6a10ca91add3b841f4
Marc Pekoua <marc.pekoua@futurenext.de>
2021-05-26 17:03:34 +0200

    add foreign keys

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,39 +1,39 @@
 from django.db import models
 
-# Create your models here.
 
-class Question(models.Model): 
+class User(models.Model):
+    Firstname = models.TextField()
+    Surname = models.TextField()
+    bild = models.CharField
     id = models.IntegerField(primary_key=True)
-    themaId = models.IntegerField()
-    userId = models.IntegerField()
+    Question = models.TextField(foreign_key=True)
+    Comment = models.TextField(foreign_key=True)
+    password = models.IntegerField()
+    Mark = models.IntegerField()
 
-class User(models.Model): 
-        Firstname = models.TextField()
-        Surname = models.TextField()
-        id = models.IntegerField(primary_key=True)
-        Question = models.TextField(foreign_key=True)
-        Comment = models.TextField(foreign_key=True)
-        password = models.IntegerField()
-        Mark = models.IntegerField()
 
- class Subject(models.Model):
-     id = models.IntegerField(primary_key=True)
-     Subject = models.TextField()
-     ProjectId = models.IntegerField(foreign_key=True)
+class Subject(models.Model):
+    id = models.IntegerField(primary_key=True)
+    Subject = models.TextField()
+    ProjectId = models.IntegerField(foreign_key=True)
 
-class Member(models.Model):
-     id = models.IntegerField(primary_key=True)
-     Name = models.TextField()
-     QuestionId = models.IntegerField(foreign_key=True)
+
+class Question(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
 
 class Project(models.Model):
-     id = models.IntegerField(primary_key=True)
-     Projectname = models.TextField()
-     NumberofMembers = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
+    Projectname = models.TextField()
+    description = models.TextField()
+    NumberofMembers = models.IntegerField()
+
 
 class Comment(models.Model):
-     id = models.IntegerField(primary_key=True)
-     UserId = models.IntegerField(foreign_key=True)
-     Comment = models.TextField()
-     Modified = models.TextField()
-     
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    Comment = models.TextField()
+    Modified = models.BooleanField
